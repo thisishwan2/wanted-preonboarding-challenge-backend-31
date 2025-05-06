2c0dfcedc1adce8da526973e364108894489c803
thisishwan2 <feel000617@gmail.com>
2025-05-06 21:30:09 +0900

    feat: JSON 필드와 Java 필드 간 자동 매핑

---
+++ src/main/java/com/example/cqrs_challenge/global/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.example.cqrs_challenge.global.config;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customJson() {
+        return builder -> builder
+                .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+}
